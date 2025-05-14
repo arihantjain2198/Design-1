0dd59654ce594b212843168fa1e03bdd44f8e05b
arihantjain2198 <31105987+arihantjain2198@users.noreply.github.com>
2025-05-13 18:13:58 -0700

    Design 1
    Design 1:
    Min stack

--- Sample.java
+++ Sample.java
@@ -1,7 +1,40 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(1)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : yes, one concept on line 27 when we use .euqlas vs == there is a difference.
 
 
-// Your code here along with comments explaining your approach
+class MinStack {
+    Stack<Integer> mStack;
+    Stack<Integer> tempStack;
+
+    public MinStack() {
+
+        mStack = new Stack<>();
+        tempStack = new Stack<>();
+    }
+
+    public void push(int val) {
+
+        mStack.push(val);
+
+        if(tempStack.isEmpty()||val <=tempStack.peek()){
+            tempStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (mStack.peek().equals(tempStack.peek())) {
+            tempStack.pop();
+        }
+        mStack.pop();
+    }
+
+    public int top() {
+        return mStack.peek();
+    }
+
+    public int getMin() {
+        return tempStack.peek();
+    }
+}
